816e0eb36ba9a8bd973aa4f787609505905238b6
Iqra <iqraanzish48@gmail.com>
2024-06-28 13:55:48 -0700

    Countdown Timer

---
+++ main.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import { differenceInSeconds } from "date-fns";
+import chalk from "chalk";
+console.log(chalk.blue("\t WELCOME TO COUNTDOWN TIMER"));
+console.log("-".repeat(60));
+function* countdownTimer(second) {
+    while (second > 0) {
+        yield second;
+        second--;
+    }
+}
+let timerIterator = countdownTimer(10);
+function displayCountdown() {
+    let result = timerIterator.next();
+    if (!result.done) {
+        const now = new Date();
+        const countdownTimer = new Date(now.getTime() + (result.value * 1000));
+        const remainingSeconds = differenceInSeconds(countdownTimer, now);
+        console.log(chalk.magenta.bold(`Remaining Seconds: ${remainingSeconds}`));
+        setTimeout(displayCountdown, 1000);
+    }
+    else {
+        console.log(chalk.greenBright("\n \tCountdown Complete!"));
+    }
+}
+displayCountdown();
